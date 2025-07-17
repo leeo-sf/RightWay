5394166815b81623860e2f2e3b3ca06410d0b4bb
Leonardo Ferreira <leonardoferreira032@gmail.com>
2025-07-17 19:52:02 -0300

    Separa propriedades não variaveis em entidade base

--- src/Core/RightWay.Data/AppDbContext.cs
+++ src/Core/RightWay.Data/AppDbContext.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using RightWay.Data.EntityTypeConfiguration;
+using RightWay.Data.EntityTypeConfiguration.Base;
 using RightWay.Domain.Entity;
+using RightWay.Domain.Entity.Base;
 
 namespace RightWay.Data;
 
@@ -9,6 +11,7 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options)
         : base(options) { }
 
+    public DbSet<BaseAddress> BaseAddress { get; set; } = default!;
     public DbSet<Address> Address { get; set; } = default!;
     public DbSet<Driver> Driver { get; set; } = default!;
     public DbSet<Order> Order { get; set; } = default!;
@@ -18,6 +21,7 @@ public class AppDbContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.ApplyConfiguration(new BaseAddressEntityTypeConfiguration());
         modelBuilder.ApplyConfiguration(new AddressEntityTypeConfiguration());
         modelBuilder.ApplyConfiguration(new DriverEntityTypeConfiguration());
         modelBuilder.ApplyConfiguration(new OrderEntityTypeConfiguration());
---
+++ src/Core/RightWay.Data/EntityTypeConfiguration/Base/BaseAddressEntityTypeConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RightWay.Domain.Entity.Base;
+
+namespace RightWay.Data.EntityTypeConfiguration.Base;
+
+internal class BaseAddressEntityTypeConfiguration : IEntityTypeConfiguration<BaseAddress>
+{
+    public void Configure(EntityTypeBuilder<BaseAddress> builder)
+    {
+        builder.ToTable("base_address");
+        builder.ConfigureBaseEntity();
+        builder.Property(c => c.ZipCode).IsRequired().HasColumnName("zip_code").HasComment("CEP");
+        builder.Property(c => c.PublicPlace).IsRequired().HasColumnName("public_place").HasComment("Logradouro");
+        builder.Property(c => c.Neighborhood).IsRequired().HasColumnName("neighborhood").HasComment("Bairro");
+        builder.Property(c => c.Locality).IsRequired().HasColumnName("locality").HasComment("Localidade");
+        builder.Property(c => c.Uf).IsRequired().HasConversion<string>().HasColumnName("uf").HasComment("Estado");
+        builder.Property(c => c.State).IsRequired().HasColumnName("state").HasComment("Estado");
+        builder.Property(c => c.Region).IsRequired().HasColumnName("region").HasComment("Região");
+        builder.Property(c => c.MunicipalCode).IsRequired().HasColumnName("municipal_code").HasComment("Código municipal");
+        builder.Property(c => c.Latitude).IsRequired().HasColumnName("latitude").HasComment("Latitude");
+        builder.Property(c => c.Longitude).IsRequired().HasColumnName("longitude").HasComment("Longitude");
+        builder.Property(c => c.Latitude).IsRequired().HasColumnName("latitude").HasComment("Latitude");
+        builder.HasMany(c => c.Addresses).WithOne(c => c.BaseAddress).HasForeignKey(c => c.BaseAddressId);
+    }
+}
